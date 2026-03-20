fdf51b26447db2f539dc5d5a484ee5b2ce7ca2f3
JulienM <julien.mury@imt-atlantique.net>
2026-03-20 09:49:44 +0100

    ﻿Merge branch 'main' of https://github.com/Alexiho/whatismyapp # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -1,9 +1,10 @@
 import "../styles/MessageForm.css";
+import sendMessage from "../index";
 
 const MessageForm = () => {
   return (
     <div className="whatismyapp-message-form">
-      <form method="post">
+      <form onSubmit={sendMessage}>
         <textarea wrap="soft" placeholder="Type your message here..."></textarea>
         <button type="submit" className="btn btn-primary">Send</button>
       </form>
